73e6e6fb4593fd695ec559cc2232150eb7cc5a7c
Matthew Caseres <matthewcaseres@outlook.com>
2020-12-18 16:42:50 -0600

    Update Card.tsx

--- components/Home/Card.tsx
+++ components/Home/Card.tsx
@@ -1,4 +1,3 @@
-import Image from 'next/image'
 type CardType = { title: string, content: string, ghLink?: string, medLink?: string}
 export default function Card({title, content, ghLink, medLink}: CardType) {
 return (
@@ -6,24 +5,26 @@ return (
     <div className="prose dark:prose-dark p-2">
       <h2 className="">{title}</h2>
       <p style={{ marginTop: -20 }}>{content}</p>
-      {(
-        <a className="ml-2" target="_blank" rel="noopener noreferrer" href={ghLink}>
-          <Image src="/012-github.png" layout="fixed" width={40} height={40} />
+      {
+        <a target="_blank" rel="noopener noreferrer" href={ghLink}>
+          <img
+            style={{ marginTop: 0 }}
+            src="/012-github.png"
+            width={40}
+            height={40}
+          />
+        </a>
+      }
+      {medLink && (
+        <a
+          className="ml-2"
+          target="_blank"
+          rel="noopener noreferrer"
+          href={medLink}
+        >
+          <img src="/019-medium.png" width={40} height={40} />
         </a>
       )}
-      {medLink && <a
-        className="ml-2"
-        target="_blank"
-        rel="noopener noreferrer"
-        href={medLink}
-      >
-        <Image
-          src="/019-medium.png"
-          layout="fixed"
-          width={40}
-          height={40}
-        />
-      </a>}
     </div>
   </div>
 );
