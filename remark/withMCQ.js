3129c6662caf5ffb7c856e58a025b7777a4b8ef4
Matthew Caseres <matthewcaseres@outlook.com>
2021-01-13 14:12:21 -0600

    misc

--- remark/withMCQ.js
+++ remark/withMCQ.js
@@ -5,10 +5,7 @@ const withMCQ = () => (tree) => {
     if (node.lang === "edtech-mcq") {
       node.type = "jsx";
       const props = yaml.safeLoad(node.value);
-      //TODO, validate JSON
-      console.log(props)
       node.value = `<MCQ {...${JSON.stringify(props)}}/>`;
-      console.log(node.value)
     }
   });
 };
