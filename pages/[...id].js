bbf8c7bb058b05e0fbf525ca536954a7d2c23eb7
Matthew Caseres <matthewcaseres@outlook.com>
2021-02-04 21:32:52 -0600

    misc

--- pages/[...id].js
+++ pages/[...id].js
@@ -1,45 +1,45 @@
-import { GetStaticPaths, GetStaticProps } from "next";
-import {
-  SideBarProvider,
-} from "../components/SideBar/SideBarContext";
+import { SideBarProvider } from "../components/SideBar/SideBarContext";
 import SideBar from "../components/SideBar/SideBar";
 import { getMdSource, getAllRoutesInfo } from "next-mdx-books";
 import renderToString from "next-mdx-remote/render-to-string";
 import hydrate from "next-mdx-remote/hydrate";
-import bookConfig from '../bookConfig.json'
-import matter from 'gray-matter'
+import bookConfig from "../bookConfig.json";
+import matter from "gray-matter";
+import slug from "remark-slug";
 
 const getProvider = (urlTree) => ({
-  component:(props) => 
+  component: (props) => (
     <SideBarProvider {...props} config={urlTree.children} />
-,
-})
+  ),
+});
 const remote = false;
 
 function Post({ urlTree, mdxSource, ghUrl, treePath }) {
-  const content = hydrate(mdxSource, {provider: getProvider(urlTree)});
+  const content = hydrate(mdxSource, { provider: getProvider(urlTree) });
   return (
-      <SideBarProvider treePath={treePath} config={urlTree.children}>
-        <SideBar ghUrl={ghUrl} treePath={treePath}>
-          <div className="prose max-w-2xl dark:prose-dark my-5 mx-2 px-8 pt-5 pb-2 bg-white shadow-md dark:bg-gray-900 rounded-xl ml-auto">
-            <div>{content}</div>
-          </div>
-        </SideBar>
-      </SideBarProvider>
+    <SideBarProvider treePath={treePath} config={urlTree.children}>
+      <SideBar ghUrl={ghUrl} treePath={treePath}>
+        <div className="prose max-w-2xl dark:prose-dark my-5 mx-2 px-8 pt-5 pb-2 bg-white shadow-md dark:bg-gray-900 rounded-xl ml-auto">
+          <div>{content}</div>
+        </div>
+      </SideBar>
+    </SideBarProvider>
   );
 }
 
 export const getStaticProps = async ({ params }) => {
   const allRoutesInfo = getAllRoutesInfo(bookConfig);
   const stringRoute = params.id.join("/");
-  const {index: nodeIndex, ghUrl, treePath} = allRoutesInfo[stringRoute]
+  const { index: nodeIndex, ghUrl, treePath } = allRoutesInfo[stringRoute];
   const urlTree = bookConfig[nodeIndex];
   const source = await getMdSource(stringRoute, allRoutesInfo, remote);
-  const {content} = matter(source)
+  const { content } = matter(source);
 
   const mdxSource = await renderToString(content, {
-
-    provider: getProvider(urlTree)
+    mdxOptions: {
+      remarkPlugins: [slug],
+    },
+    provider: getProvider(urlTree),
   });
   return {
     props: { urlTree, mdxSource, ghUrl, treePath }, // will be passed to the page component as props
