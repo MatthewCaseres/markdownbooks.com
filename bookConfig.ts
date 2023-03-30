13cab286331ba1de6d024edb547b3c386ef43e72
dylviz <dylanvizcarra@gmail.com>
2023-03-30 11:00:34 -0700

    removed broken links for dev. Updated script- commented out non-working links. Upgraded package versions to remedy bug

--- bookConfig.ts
+++ bookConfig.ts
@@ -1,47 +1,46 @@
-import { summaryToUrlTree, UserFunction, getAllRoutesInfo } from "github-books"
-import fs from 'fs'
-import visit from 'unist-util-visit'
-import GithubSlugger from 'github-slugger'
+import { summaryToUrlTree, UserFunction, getAllRoutesInfo } from "github-books";
+import fs from "fs";
+import visit from "unist-util-visit";
+import GithubSlugger from "github-slugger";
 
-let allHeaders: Record<string, any> = {}
-export type HeadersConfig = {depth: number, title: string, slug: string}[]
+let allHeaders: Record<string, any> = {};
+export type HeadersConfig = { depth: number; title: string; slug: string }[];
 
 const headersFunction: UserFunction = ({ treeNode, mdast }) => {
-  console.log(treeNode)
+  console.log("treeNode: " + treeNode);
   const routePrefix = treeNode.route;
   var slugger = new GithubSlugger();
-  const headers: HeadersConfig = []
+  const headers: HeadersConfig = [];
   for (let node of mdast.children) {
-    if(node.type === "heading" && (node.depth === 2 || node.depth === 3)) {
-      const depth = node.depth
-      const title = node.children[0].value
-      const slug = slugger.slug(title)
-      headers.push({depth, title, slug})
+    if (node.type === "heading" && (node.depth === 2 || node.depth === 3)) {
+      const depth = node.depth;
+      const title = node.children[0].value;
+      const slug = slugger.slug(title);
+      headers.push({ depth, title, slug });
     }
   }
-  allHeaders[routePrefix] = headers
-}
+  allHeaders[routePrefix] = headers;
+};
 
 (async () => {
   // const tsTree = await summaryToUrlTree({
   //   url: "https://github.com/basarat/typescript-book/blob/master/SUMMARY.md",
   //   userFunction: headersFunction
   // })
-  const osTree = await summaryToUrlTree({
-    url: "https://github.com/Open-EdTech/library/blob/main/OMSCS-OS.md",
-    userFunction: headersFunction
-  })
+  // const osTree = await summaryToUrlTree({
+  //   url: "https://github.com/MatthewCaseres/AWS-Notes/tree/main/source/OMSCS-OS.md",
+  //   userFunction: headersFunction,
+  // });
   const awsTree = await summaryToUrlTree({
-    url: "https://github.com/Open-EdTech/AWS-Associate-Notes/blob/main/00-index.md",
-    userFunction: headersFunction
-  })
-  const bibleTree = await summaryToUrlTree({
-    url: "https://github.com/Open-EdTech/library/blob/main/Bible-KJV.md",
+    url: "https://raw.githubusercontent.com/MatthewCaseres/AWS-Notes/main/source/00-index.md",
     userFunction: headersFunction,
-  })
-  fs.writeFileSync('bookPageHeadings.json', JSON.stringify(allHeaders))
-  fs.writeFileSync('bookConfig.json', JSON.stringify([osTree, bibleTree, awsTree]))
+  });
+  // const bibleTree = await summaryToUrlTree({
+  //   url: "https://github.com/Open-EdTech/library/blob/main/Bible-KJV.md",
+  //   userFunction: headersFunction,
+  // });
+  fs.writeFileSync("bookPageHeadings.json", JSON.stringify(allHeaders));
+
+  fs.writeFileSync("bookConfig.json", JSON.stringify([awsTree]));
   // fs.writeFileSync('bookConfig.json', JSON.stringify([awsTree]))
 })();
-
-
